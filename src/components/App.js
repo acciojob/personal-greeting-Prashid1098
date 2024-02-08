0ece91ed89be8a35fc15dd44e383855f9d230d5c
Prashid Pal <67044025+Prashid1098@users.noreply.github.com>
2024-02-09 00:56:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+    let [name,setName]=useState("");
+
+    function updateName(event)
+    {
+        setName(event.target.value);
+    }
   return (
     <div>
         {/* Do not remove the main div */}
+        <div>Enter your name:</div>
+        <input type="text" id="abc" onChange={updateName} />
+        <p id="abc">{name}</p>
     </div>
   )
 }
